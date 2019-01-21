dd39be29159f1798991e2d02f36c644904cc7e97
sulivansimoes <sulivansimoes@gmail.com>
2019-01-20 22:00:07 -0200

    criado função para formatar input de cpf no momento de preencher o campo

--- src/app/usuario/componentes/login.component.ts
+++ src/app/usuario/componentes/login.component.ts
@@ -10,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  private user:String = "";
+  private senha:String = "";
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {  }
 
+  /**
+   * @description: adiciona caracteres pertencentes a mascara do cpf, de forma automatica.
+   * @param {String} cpf - número do cpf 
+   */
+  private formataCpf(cpf:any){
+ 
+    if( isNaN( cpf.charAt(cpf.length-1) )  ){        
+      // retira caracter se não for número
+      this.user = this.user.substring(0, this.user.length-1);    
+    }else{
+      switch(cpf.length){
+        case 3 : case 7 :
+          this.user += "."
+        break;
+        case 11 :
+          this.user +="-"
+        break;
+      }
+    }
+  }
 }
